87eaa392b94c1ed1b4b46f46a9ddb8e798cdaae3
ifugiwara11 <ifugiwara11@uolinc.com>
2024-05-13 19:27:36 -0300

    email try commit

--- script.js
+++ script.js
@@ -14,3 +14,19 @@ function toggleMenu() {
     var menu = document.querySelector('.menu');
     menu.classList.toggle('active');
 }
+
+
+
+// EMAIL
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    emailjs.init('Igor Fugiwara'); // Substitua 'YOUR_USER_ID' pelo seu User ID do EmailJS
+
+    emailjs.sendForm('4dL0a-QFs8e2zrvDS', 'kef3h9DU8BAdYK5UlaISn', this)
+        .then(function() {
+            alert('Email enviado com sucesso!');
+        }, function(error) {
+            alert('Ocorreu um erro ao enviar o email: ' + JSON.stringify(error));
+        });
+});

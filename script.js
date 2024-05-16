a9594491bf7ddbef58b4881784717e224d0fa2f4
ifugiwara11 <ifugiwara11@uolinc.com>
2024-05-16 19:07:06 -0300

    atualizando com sistema de email

--- script.js
+++ script.js
@@ -9,21 +9,31 @@ const swiper = new Swiper('.swiper-container', {
     },
 });
 
+const swiperGallery = new Swiper('.gallery.swiper-container', {
+    loop: true,
+    autoplay: {
+        delay: 3000, // 3 segundos
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+
 
 function toggleMenu() {
     var menu = document.querySelector('.menu');
     menu.classList.toggle('active');
 }
 
-
-
 // EMAIL
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
 
-    emailjs.init('Igor Fugiwara'); // Substitua 'YOUR_USER_ID' pelo seu User ID do EmailJS
+    emailjs.init('4dL0a-QFs8e2zrvDS'); // Substitua 'YOUR_USER_ID' pelo seu User ID do EmailJS
 
-    emailjs.sendForm('4dL0a-QFs8e2zrvDS', 'kef3h9DU8BAdYK5UlaISn', this)
+    emailjs.sendForm('service_yky4tkg', 'template_cmkmklm', this)
         .then(function() {
             alert('Email enviado com sucesso!');
         }, function(error) {

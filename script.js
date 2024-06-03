bb2282f8d6552b3aed39e11f4c22b4a38efa5e02
ifugiwara11 <ifugiwara11@uolinc.com>
2024-06-03 19:30:06 -0300

    menu

--- script.js
+++ script.js
@@ -24,11 +24,36 @@ const swiperGallery = new Swiper('.gallery.swiper-container', {
     spaceBetween: 0 // Espaço entre as imagens
 });
 
-
+// MENU
 function toggleMenu() {
-    document.querySelector('.menu').classList.toggle('active');
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+
+    const menuToggle = document.querySelector('.menu-toggle');
+    if (menu.classList.contains('active')) {
+        menuToggle.classList.add('fixed');
+    } else {
+        menuToggle.classList.remove('fixed');
+    }
 }
 
+function adjustMenuPosition() {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const rect = menuToggle.getBoundingClientRect();
+    const topOffset = rect.top + window.pageYOffset;
+
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+    // Adiciona a classe 'fixed' ao menu hamburger quando a rolagem da tela ultrapassar a posição do menu
+    if (scrollTop > topOffset) {
+        menuToggle.classList.add('fixed');
+    } else {
+        menuToggle.classList.remove('fixed');
+    }
+}
+
+// Verifica a posição do menu ao rolar a tela
+window.addEventListener('scroll', adjustMenuPosition);
 // EMAIL
 document.getElementById('contact-form').addEventListener('submit', function(event) {
     event.preventDefault();
